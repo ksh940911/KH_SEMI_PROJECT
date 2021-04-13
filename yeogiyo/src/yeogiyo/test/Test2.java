d42c77a75203ab681eb372aa1ab2ecd906a6402c
jsungl <wotjd931026@gmail.com>
2021-04-14 03:10:20 +0900

    Update Test2.java

--- yeogiyo/src/yeogiyo/test/Test2.java
+++ yeogiyo/src/yeogiyo/test/Test2.java
@@ -3,6 +3,7 @@ package yeogiyo.test;
 public class Test2 {
 
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		System.out.println("test1");
 		System.out.println("test2");
 		System.out.println("test3");
@@ -12,6 +13,10 @@ public class Test2 {
 		System.out.println("test7");
 		System.out.println("test9");
 		System.out.println("test10");
+=======
+		System.out.println("test³¡");
+		
+>>>>>>> Stashed changes
 		
 	}
  
