8a24d30a2c218305dec36d2c86d21e337874395a
jsungl <wotjd931026@gmail.com>
2021-04-14 03:15:12 +0900

    Update Test2.java

--- yeogiyo/src/yeogiyo/test/Test2.java
+++ yeogiyo/src/yeogiyo/test/Test2.java
@@ -3,18 +3,7 @@ package yeogiyo.test;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("test1");
-		System.out.println("test2");
-		System.out.println("test3");
-		System.out.println("test4");
-		System.out.println("test5");
-		System.out.println("test6");
-		System.out.println("test7");
-		System.out.println("test9");
-		System.out.println("test10");
-		System.out.println("test11");
-		System.out.println("test12");
-		System.out.println("test13");
+		System.out.println("³¡");
 		
 		
 	}
