ebfdf3ae28654f46de949e61e3195d678c842170
jsungl <wotjd931026@gmail.com>
2021-04-13 23:48:47 +0900

    sdfsd

--- yeogiyo/src/yeogiyo/test/Test2.java
+++ yeogiyo/src/yeogiyo/test/Test2.java
@@ -10,14 +10,11 @@ public class Test2 {
 		System.out.println("test5");
 		System.out.println("test6");
 		System.out.println("test7");
-<<<<<<< Updated upstream
 		System.out.println("test8");
-=======
 		System.out.println("test8");		
 		System.out.println("test9");
 		System.out.println("test10");
-		
->>>>>>> Stashed changes
+
 	}
  
 }
