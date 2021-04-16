c1f3ca11332d4c86264799d478ee1205e13a5bb2
jsungl <wotjd931026@gmail.com>
2021-04-16 16:06:32 +0900

    메인페이지 수정1

--- yeogiyo/src/member/model/service/MemberService.java
+++ yeogiyo/src/member/model/service/MemberService.java
@@ -1,5 +1,22 @@
 package member.model.service;
 
+import java.sql.Connection;
+
+import member.model.dao.MemberDao;
+import member.model.vo.Member;
+import static common.JDBCTemplate.*;
+
 public class MemberService {
+
+	private MemberDao memberDao = new MemberDao();
+	
+	
+	//아이디로 멤버조회
+	public Member selectOne(String memberId) {
+		Connection conn = getConnection();
+		Member member = memberDao.selectOne(conn, memberId);
+		close(conn);
+		return member;
+	}
 
 }
