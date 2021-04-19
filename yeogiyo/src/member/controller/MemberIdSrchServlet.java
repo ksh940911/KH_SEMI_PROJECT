36a7beb9200b86cb9fdbc0381611a4669dc9fcbc
jsungl <wotjd931026@gmail.com>
2021-04-19 15:47:39 +0900

    아이디및비번찾기 작업중

---
+++ yeogiyo/src/member/controller/MemberIdSrchServlet.java
@@ -0,0 +1,36 @@
+package member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberIdSearchServlet
+ */
+@WebServlet("/member/searchId")
+public class MemberIdSrchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/member/searchId.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//사용자 입력값 처리
+		
+		//db에서 조회
+		
+		
+	}
+
+}
--- yeogiyo/src/member/model/dao/MemberDao.java
+++ yeogiyo/src/member/model/dao/MemberDao.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
 import static common.JDBCTemplate.*;
 
