3ce0e1c27c9c4fbb9b8b2abe5b80a0c28d0b0183
kym9129 <kym9129@naver.com>
2021-04-20 22:33:34 +0900

    feat : 주문테이블에 주문정보 담기

--- yeogiyo/src/order/model/dao/OrderDao.java
+++ yeogiyo/src/order/model/dao/OrderDao.java
@@ -26,10 +26,34 @@ public class OrderDao {
 
 	public int insertOrder(Connection conn, Order order) {
 		int result = 0;
+		String sql = prop.getProperty("insertOrder");
+		PreparedStatement pstmt = null;
 		
-		
-		
-		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			//order_id, member_id, res_id, order_date, address, address_sub, phone, order_comment, payment_way, 
+			//payment_place, order_menu, total_price
+			//insert into tb_order values(sql_tb_order_id.nextval, ?, ?, sysdate, ?, ?, ?, ?, ?, ?, ?, ?)
+			
+			pstmt.setInt(1, order.getOrderId());
+			pstmt.setString(2, order.getMemberId());
+			pstmt.setInt(3, order.getResId());
+			pstmt.setString(4, order.getAddress());
+			pstmt.setString(5, order.getAddressSub());
+			pstmt.setString(6, order.getPhone());
+			pstmt.setString(7, order.getOrderComment());
+			pstmt.setString(8, order.getPaymentWay());
+			pstmt.setString(8, order.getPaymentPlace());
+			pstmt.setString(9, order.getOrderMenu());
+			pstmt.setInt(10, order.getTotalPrice());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
 		
 		return result;
 	}
@@ -56,6 +80,41 @@ public class OrderDao {
 		
 		
 		return totalOrder;
+	}
+
+	public Order selectLastOrderById(Connection conn, String memberId) {
+		Order order = new Order();
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		String query = prop.getProperty("selectLastOrderById");
+		
+		try{
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, memberId);
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				/*
+				 * order_id, member_id, res_id, order_date, address, address_sub, phone, order_comment, payment_way, 
+					payment_place, order_menu, total_price
+				 * */
+				order.setOrderId(rset.getInt("order_id"));
+				order.setMemberId(rset.getString("member_id"));
+				order.setResId(rset.getInt("res_id"));
+			}
+			
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			close(rset);
+			close(pstmt);
+		}
+		
+		
+		return order;
 	}
 
 }
